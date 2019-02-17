f793be15bfed140e8b2b02c4aa22f23e91890772
DeepThind <47137807+DeepThind@users.noreply.github.com>
2019-02-16 19:09:49 -0800

    Add routes

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+app.get('/',(req,res,next)=>{
+    res.send("Hello There");
+    res.end();
+})
+app.post('/create', (req,res,next)=>{
+
+})
+app.post('/create', (req,res,next)=> {
+
+})
+
+const port = process.env.port || 3000;
+app.listen(port, ()=> console.log(`Listen to port ${port}`));
